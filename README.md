184944840426845b0f0e8ce3da6fdaa72c8e97a7
Antoniofo <antonio57201@gmail.com>
2022-03-15 21:16:49 +0100

    Clean up

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # WarnSystem
- Plugin that add a warn system on your server. You will need database activated in your synapse config.
-# WarnSystemV2
-WarnSystemV2 is a second WarnSystem that use no DBO and only Synapse native database Put do not work for now
+WarnSystemV2 is a second WarnSystem that use no DBO and only Synapse native database.
+
+## Installation
+
+1. [Install Synapse](https://github.com/SynapseSL/Synapse/wiki#hosting-guides).
+2. Place the [dll](https://gitub.com/VT-DevGit/WarnSystem/releases/) file in your plugin directory.
+3. Restart/Start your server.
+
+## Specification
+
+This system can go up to infinite warn per player. It display a broadcast to the player with a message (configurable).
+
+## Permission
+This plugin only contains one permission, for the command to warn other player. You can remove, add and see warn. Players are also able to see there warn in the client console with `.warning` command that doesn't require any permission.
+
+```
+- ws.warn	: add/see/remove warns
+```
--- WarnSystem/Plugin.cs
+++ WarnSystem/Plugin.cs
@@ -1,21 +1,5 @@
-﻿using Synapse;
-using Synapse.Api;
-using Synapse.Api.Events.SynapseEventArguments;
-using Synapse.Api.Plugin;
-using Synapse.Database;
+﻿using Synapse.Api.Plugin;
 using Synapse.Translation;
-using System.Threading.Tasks;
-using WarnSystem.DataBase;
-
-/*                      /!\ Warning /!\ 
- * The synapse dll is not the synapse from the nugets
- * but the synapse form tempLib because it is not the right vection !
- * [Fr]
- * La dll synapse n'est pas à la bonne verstion donc j'ai pris
- * un dll de tempLib qui est a suprimer lorsque les nugets seront a jours !
- * 
- * @Antoniofo
- */                         
 
 namespace WarnSystem
 {
@@ -24,72 +8,43 @@ namespace WarnSystem
         Author = "Antoniofo",
         Description = "Add a warn system",
         LoadPriority = 0,
-        SynapseMajor = 2,
-        SynapseMinor = 8,
-        SynapsePatch = 0,
+        SynapseMajor = SynapseController.SynapseMajor,
+        SynapseMinor = SynapseController.SynapseMinor,
+        SynapsePatch = SynapseController.SynapsePatch,
         Version = "v1.0.0"
         )]
     public class Plugin : AbstractPlugin
     {
-        [API]
-        public static WarnPlayerRepository WarnRepository { get; private set; }
-
         [SynapseTranslation]
-        public static new SynapseTranslation<PluginTranslate> Translation { get; set;}
-
-        [Config(section = "WarnSystem")]
-        public static PluginConfig Config { get; set; }
-
-        public static bool DataBaseEnabled { get; private set; }
-
+        public static new SynapseTranslation<PluginTranslation> Translation { get; set; }
         public override void Load()
         {
-            DatabaseManager.CheckEnabledOrThrow();
-            
-
-            WarnRepository = new WarnPlayerRepository();
-            DataBaseEnabled = true;
-            Server.Get.Events.Player.PlayerJoinEvent += OnJoin;
-            
-            InitTranslation();
-
             base.Load();
-        }
-
-        private void InitTranslation()
-        {
-            Translation.AddTranslation(new PluginTranslate());
-            Translation.AddTranslation(new PluginTranslate
+            Translation.AddTranslation(new PluginTranslation());
+            Translation.AddTranslation(new PluginTranslation
             {
-                WarningMessage = "<color=red>Attention certaine de vos données seront stocker pour la sécuriter du server.\n Contactez le staff du server pour plus d'informations</color>"
+                Warned = "Vous avez reçu un avertissement pour %reason%",
+                WarnSuccess = "Le joueur %player% a reçu un avertissement pour %reason%",
+                Remove = "Avertissement retiré",
+                PlayerNotFound = "Joueur non trouvé",
+                ArgsError = "Argument insuffusant",
+                TypeError = "Paramètre Invalide essayez avec see/add/remove",
+                NoWarn = "Le joueur n'a pas d'avertissement",
+                WarnNotFound = "Avertissement non trouvé"
+
             }, "FRENCH");
-            Translation.AddTranslation(new PluginTranslate
+            Translation.AddTranslation(new PluginTranslation
             {
-                WarningMessage = "<color=red>Bitte beachten Sie, dass einige Ihrer Daten gespeichert werden, um sie vom Server abzusichern.\n Kontaktieren Sie das Serverpersonal für weitere Informationen</color>"
+                Warned = "Sie haben eine Warnung erhalten für %reason%",
+                WarnSuccess = "Spieler %player% erhielt eine Verwarnung für %reason%",
+                Remove = "Werbung entfernt",
+                PlayerNotFound = "Spieler nicht gefunden",
+                ArgsError = "Nicht genug Argumente",
+                TypeError = "Ungültiger Parameter Versuch mit see/add/remove",
+                NoWarn = "Der Spieler hat keine Warnung",
+                WarnNotFound = "Warnung nicht gefunden"
             }, "GERMAN");
-        }
 
-        private void OnJoin(PlayerJoinEventArgs ev)
-        {
-            if (WarnRepository.TryGetByUserId(ev.Player.UserId, out WarnDbo dbo))
-            {
-                if (dbo.NickName != ev.Player.NickName)
-                {
-                    dbo.NickName = ev.Player.NickName;
-                    WarnRepository.UpdateOrAdd(dbo);
-                }
-            }
-            else
-            {
-                dbo = new WarnDbo(ev.Player.UserId)
-                {
-                    NickName = ev.Nickname,
-                };
-                WarnRepository.UpdateOrAdd(dbo);
-
-                // do the warning message for eu server
-                ev.Player.OpenReportWindow(Translation.ActiveTranslation.WarningMessage);
-            }
         }
     }
 }
