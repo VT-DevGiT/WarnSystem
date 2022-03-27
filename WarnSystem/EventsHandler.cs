5b620322e6ea08f332562d1b79b56b5efe7da870
Antoniofo <antonio57201@gmail.com>
2022-03-27 11:41:52 +0200

    Fix Bug Double accès a la db

--- WarnSystem/EventsHandler.cs
+++ WarnSystem/EventsHandler.cs
@@ -1,4 +1,5 @@
-﻿using Synapse.Api.Events.SynapseEventArguments;
+﻿using Synapse.Api;
+using Synapse.Api.Events.SynapseEventArguments;
 using Synapse.Database;
 
 namespace WarnSystem
@@ -12,16 +13,21 @@ namespace WarnSystem
 
         private void OnJoin(PlayerJoinEventArgs ev)
         {
+            MEC.Timing.CallDelayed(0.1f, () => OnJoinDelayed(ev.Player));
+        }
+
+        private void OnJoinDelayed(Player ply)
+        {
             if (!Plugin.Config.DisclamerAtFirstConnection)
                 return;
 
-            PlayerDbo dbo = DatabaseManager.PlayerRepository.FindByGameId(ev.Player.UserId);
+            PlayerDbo dbo = DatabaseManager.PlayerRepository.FindByGameId(ply.UserId);
             if (!Plugin.WarnIsSet(dbo))
             {
                 Plugin.SetNumberOfWarns(dbo, 0);
                 DatabaseManager.PlayerRepository.Save(dbo);
 
-                ev.Player.SendBroadcast(5, Plugin.Translation.ActiveTranslation.WarnDnt);
+                ply.SendBroadcast(5, Plugin.Translation.ActiveTranslation.WarnDnt);
             }
         }
     }
